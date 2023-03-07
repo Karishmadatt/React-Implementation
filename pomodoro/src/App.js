05676a7be9a6e73a58a904372cdedd017a6d0dbb
Karishmadatt <karishmadatt2000@gmail.com>
2023-03-07 16:23:01 +0530

    on-click start function[working of start tab]

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -2,13 +2,25 @@ import React, { useState } from 'react';
 import './App.css';
 
 function padtime (time){
-  console.log('try');
+  // console.log('try');
   return time.toString().padStart(2,'0');
 }
 
-export default function App() {
 
- const[timeleft , settime] = useState(25*60);
+export default function App() {
+  
+  const[title , settitle] = useState('Start the timmer!')
+  const[timeleft , settime] = useState(10);
+  
+  function startTimmer(){
+    setInterval(()=>{
+         settime (timeleft=>{
+          if(timeleft>=1) return timeleft-1
+          //reset the timer;
+          return 0;
+        });
+    },1000)
+  }
 
  const min = Math.floor(timeleft/60);
  const sec = padtime(timeleft - min*60);
@@ -16,7 +28,7 @@ export default function App() {
   return (
     
     <div className="app">
-      <h2>Pomodoro!</h2>
+      <h2>{title}</h2>
         {/* <p>hello</p>  */}
       <div className="timer">
         <span>{min}</span>
@@ -25,7 +37,7 @@ export default function App() {
       </div>
 
       <div className="buttons">
-        <button>Start</button>
+        <button onClick={startTimmer}>Start</button>
         <button>Stop</button>
         <button>Reset</button>
       </div>
