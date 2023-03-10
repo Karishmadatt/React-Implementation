c4d417c722756e76e857f9b28f3028a97947deb9
Karishmadatt <karishmadatt2000@gmail.com>
2023-03-10 19:29:24 +0530

    Working with HTML,CSS[bulma]

--- implementing_react_component/src/App.js
+++ implementing_react_component/src/App.js
@@ -1,18 +1,51 @@
+import 'bulma/css/bulma.css';
 import Profilecard from "./Profilecard";
-import AlexaImage from "./images/alexa.png"
-import CortanaImage from "./images/cortana.png"
-import SiriImage from "./images/siri.png"
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App(){
     return(
     <div>
-        <div>Personal Digital Assistants</div>
+        <section className='hero is-primary'>
+            <div className='hero-body'>
+                <p className='title'>
+                Personal Digital Assistants
+                </p>
+            </div>
+        </section>
 
         {/* <img src={AlexaImage} /> */}
-
-        <Profilecard title = 'Alexa' handle = '@alexa99' image={AlexaImage}/>
-        <Profilecard title = 'Cortana' handle = '@cortana32' image={CortanaImage}/>
-        <Profilecard title = 'Siri' handle = '@siri01' image={SiriImage}/>
+        <div className='container'>
+            <section className='section'>
+                <div className='columns'>
+                    <div className='column is-3'>
+                      <Profilecard 
+                      title = 'Alexa' 
+                      handle = '@alexa99'
+                      image={AlexaImage}
+                      description='Alexa was created by Amazon and is used to buy things.'
+                      />
+                    </div>
+                    <div className='column is-3'>
+                      <Profilecard 
+                      title = 'Cortana' 
+                      handle = '@cortana32' 
+                      image={CortanaImage}
+                      description='Cortana was made by Microsoft. Who knows what is does?'
+                      />
+                    </div>
+                    <div className='column is-3'>
+                      <Profilecard 
+                      title = 'Siri' 
+                      handle = '@siri01' 
+                      image={SiriImage}
+                      description='Siri is made by Apple and is being phase out.'
+                      /> 
+                    </div>
+                </div>
+            </section>
+        </div>
     </div>
     )
 }
--- implementing_react_component/src/Profilecard.js
+++ implementing_react_component/src/Profilecard.js
@@ -1,10 +1,19 @@
-function Profilecard({title,handle,image}){//using argument destructuring directly instead of writing props
+function Profilecard({title,handle,image,description}){//using argument destructuring directly instead of writing props
     // const {title,handle} = props //using argument destructuring
     return (
-        <div>
-            <img src={image} />
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
+        <div className="card">
+            <div  className="card-image">
+            <figure className="image is-1by1">
+              <img src={image} />
+            </figure>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">Title is {title}</p>
+                    <p className="subtitle is-6">Handle is {handle}</p>
+                </div>
+                <div className="content">{description}</div>
+            </div>
         </div>
     )
 }
